06cf6c451e42baca167ac0cadc402287e11f73c9
Jehnean <jehnean@gmail.com>
2016-02-03 11:35:08 -0800

    Initial game commit.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+	/* Initializes the game */
+	
+	/* Notes from meeting with Ilias 
+
+	var game = new Board(); 
+	init method 
+	game.cells.push(new Cell(100-i)) 
+
+	$(".cell").forEach(function(cell, i)) {
+		cell.attribute("id", 100-i)
+		game.cells.push(newCell(100-i))
+	}
+	*/
+
+function createBoard(){ 	
+	for (var i = 1; i < 10; i++) {
+		for (var j = 1; j < 10; j++) {
+			$("#row"+i).append('<td class="r'+i+' c'+j+'></td>' );
+		}
+	}
+
+}

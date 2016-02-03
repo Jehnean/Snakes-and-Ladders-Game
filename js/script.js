1cbc45df84252b23dbbe4e4253fb671d342f7839
Jehnean <jehnean@gmail.com>
2016-02-03 11:57:21 -0800

    Add IDs to cells

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
+/* Initializes the game */
 
+/* Notes from meeting with Ilias 
 
-	/* Initializes the game */
-	
-	/* Notes from meeting with Ilias 
+var game = new Board(); 
+init method 
+game.cells.push(new Cell(100-i)) 
 
-	var game = new Board(); 
-	init method 
-	game.cells.push(new Cell(100-i)) 
-
-	$(".cell").forEach(function(cell, i)) {
-		cell.attribute("id", 100-i)
-		game.cells.push(newCell(100-i))
-	}
-	*/
-
-function createBoard(){ 	
-	for (var i = 1; i < 10; i++) {
-		for (var j = 1; j < 10; j++) {
-			$("#row"+i).append('<td class="r'+i+' c'+j+'></td>' );
-		}
-	}
-
+$(".cell").forEach(function(cell, i)) {
+	cell.attribute("id", 100-i)
+	game.cells.push(newCell(100-i))
 }
+*/
+
+function setCellIds() {
+	$.each($(".cell"), function(index, cellElement) {
+	  var id = index + 1; 
+	  $(cellElement).attr("id", id); 
+	});
+}
+
+// function createBoard(){ 	
+// 	for (var i = 1; i < 10; i++) {
+// 		for (var j = 1; j < 10; j++) {
+// 			$("#row"+i).append('<td class="r'+i+' c'+j+'></td>' );
+// 		}
+// 	}
+
+// }

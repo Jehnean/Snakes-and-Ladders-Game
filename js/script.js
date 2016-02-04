d74f24677c65680aaa3037f21005e1b884aaff4b
Jehnean <jehnean@gmail.com>
2016-02-03 16:26:12 -0800

    Adding button click activates player state.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
-/* Initializes the game */
 
-/* Notes from meeting with Ilias 
 
-var game = new Board(); 
-init method 
-game.cells.push(new Cell(100-i)) 
-
-$(".cell").forEach(function(cell, i)) {
-	cell.attribute("id", 100-i)
-	game.cells.push(newCell(100-i))
-}
-*/
-
+// Assign IDs to Cells 
 function setCellIds() {
-	$.each($(".cell"), function(index, cellElement) {
-	  var id = index + 1; 
-	  $(cellElement).attr("id", id); 
-	});
+    // Each .cell look at index and cellElement
+$.each($(".cell"), function(index, cellElement) {
+    // iterate through index
+    var id = index + 1; 
+    // Append with the id and at p text 
+    $(cellElement).attr("id", id).append('<p>'+id+'</p>'); 
+  });
 }
 
-// function createBoard(){ 	
-// 	for (var i = 1; i < 10; i++) {
-// 		for (var j = 1; j < 10; j++) {
-// 			$("#row"+i).append('<td class="r'+i+' c'+j+'></td>' );
-// 		}
-// 	}
+// Function for roll 
+function roll() {
+    // store roll result of 1-6 
+    var rollResult = Math.floor((Math.random()*6)+1);
+    // add roll result to id number and add player class
+    $('#'+rollResult).addClass('player');  
+}
+// Function for new turn and current position 
+function turn(curPos) {
+  var newPos = curPos+roll(); 
+  alert("Made it to here!"); 
+}
 
-// }
+function rollDice() {
+  $('.roll-dice').on('click', function() {
+      roll(); 
+    }); 
+}
+
+
+// make player current position 
+// Set current position to -1 
+// make dieRoll with Math.floor(Math.random() * 6) + 1;
+// add results of dieRoll to player current position 
+// highlight cell id of player current position 

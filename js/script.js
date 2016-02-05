f33c5c273d6b1bd2b43fa0d650943904481554be
Jehnean <jehnean@gmail.com>
2016-02-04 16:17:53 -0800

    Adding broken version with SnakesAndLadders object

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var player = 0; 
-var currentPosition = 0; 
+var currentPosition = 0;
+var rollResult;
 
 // Assign IDs to Cells 
 function setCellIds() {
@@ -12,43 +13,73 @@ $.each($(".cell"), function(index, cellElement) {
   });
 }
 
+// Snakes 
+
+var SnakesAndLadders = {
+    // key : value
+    12 : 16, 
+    13 : 48, 
+    14 : 48, 
+    19 : 60, 
+    55 : 76,
+    78 : 97,
+    99: 29,
+    47: 18,
+    25: 7
+};
+
+// var snakeOrLadderSpace = Object.keys(SnakesAndLadders); 
+
+// function checkForSnakeOrLadder(nextPosition) {
+//   if (snakeOrLadderSpace.indexOf(nextPosition) >= 0 ) {
+//     console.log("HIT"); 
+//     nextPosition = SnakesAndLadders[nextPosition]; 
+//   }
+//   return nextPosition; 
+// }
+
+
+function setupPlayer() { 
+  // If player position is 0 (not on the board)
+      // highlight cell id of player current position 
+      $('#'+rollResult).addClass('player');
+      // player is equal to the roll result 
+      player = rollResult;
+}
+
+
 // Function for roll 
 function roll() {
     // store roll result of 1-6 
-    var rollResult = Math.floor((Math.random()*6)+1);
-    if (player === 0) {
-      $('#'+rollResult).addClass('player');
-      player = rollResult; 
-    } else {
-      // var for current position and store current position
-      currentPosition = $('.player').attr('id');
-      console.log(currentPosition);
-      // current position + new 
-      var nextPosition = parseInt(currentPosition) + rollResult;
-      console.log(nextPosition);
-      // Clear player cell 
-      $('.player').removeClass('player'); 
-      // make new position .player 
-      $('#'+nextPosition).addClass('player');
+    rollResult = Math.floor((Math.random()*6)+1);
+    // IF the player is not on the board
+    if ( player === 0 ) {
+      setupPlayer(); 
     }
+    
+    // var for current position to get the player class and assign the id 
+    currentPosition = $('.player').attr('id');
+    // log the current position 
+    console.log(currentPosition);
+    // current position + new 
+    var nextPosition = parseInt(currentPosition) + rollResult;
+    // If your next position is more than 100, alert
+    if (nextPosition >= 100) {
+      alert("YOU WIN"); 
+    };
+  // Saving the next position in the console
+    console.log(nextPosition);
+    // nextPosition = checkForSnakeOrLadder(nextPosition); 
+    // Clear player cell 
+    $('.player').removeClass('player'); 
+    // make new position .player 
+    // add new value to stored current position 
+    $('#'+nextPosition).addClass('player');
 }
 
-// Function for new turn and current position 
-function turn(curPos) {
-  var newPos = curPos+roll(); 
-  alert("Made it to here!"); 
-}
-
+// Function for calling roll() on click 
 function rollDice() {
   $('.roll-dice').on('click', function() {
       roll(); 
     }); 
 }
-
-
-// add results of dieRoll to player current position 
-// highlight cell id of player current position 
-
-
-// on click clear current position of player class 
-// add new value to stored current position 

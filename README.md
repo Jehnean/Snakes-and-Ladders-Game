2dd379403406c648149ae25c8735e87170c10cb1
Jehnean <jehnean@gmail.com>
2016-02-05 10:19:19 -0800

    User can now see a graphic board. Added screen shots of code to discuss.

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
-# hmwk-microblog
+#Synopsis
 
-Place useful instructioner here. 
+JavaScript Snakes and Ladders Game project for GA WDI 26. A simple Snakes and Ladders game.
+
+#Code Example
+
+Show what the library does as concisely as possible, developers should be able to figure out how your project solves their problem by looking at the code example. Make sure the API you are showing off is obvious, and that your code is short and concise.
+
+#Motivation
+
+Creating an MVP of this game to improve upon later.
+
+#Installation
+
+In browser based game. No installation required.
+
+#API Reference
+
+N/A
+
+#Tests
+
+N/A
+
+#Contributors
+
+@Jehnean
+
+#License
+
+N/A
+
+#Project Sepcific Catagories
+
+###Project Workflow: Did you complete the user stories, wireframes, task tracking, and/or ERDs, as specified above? Did you use source control as expected for the phase of the program you’re in (detailed above)?
+
+Yes - Created User stories: 
+
+As a player, I want to know how to play the game so I can formulate a strategy to win. 
+As a player, I want to know the criteria for winning the game. 
+As a player, I want to know the rules so I know what I can and can’t do. 
+As a player, I want at least one opponent to play against. 
+
+As a player, I want to be able to reset the game so I can start over.
+As a player, I want to be able to see all the pieces on the board so I can see where I rank. 
+As a player, I want to be able to clearly see which piece is mine. 
+As a player, I want to roll the dice so I can move forward. 
+As a player, I want to see who has won. 
+As a player, I want be notified if the game has ended, and who won. 
+As a player, I want to land on ladder spaces to advance more quickly. 
+As a player, I want to avoid snake spaces to so I don’t fall behind. 
+
+
+###Technical Requirements: Did you deliver a project that met all the technical requirements? Given what the class has covered so far, did you build something that was reasonably complex?
+
+Yes - Render a game in the browser
+Yes - Design logic for winning & visually display which player won 
+Yes - Include separate HTML / CSS / JavaScript files
+Yes - Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
+Yes - Use Javascript or jQuery for DOM manipulation
+Yes - Deploy your game online, where the rest of the world can access it (there will be a quick tutorial on this later in the week)
+Yes - Use semantic markup for HTML and CSS (adhere to best practices)
+
+###Creativity: Did you added a personal spin or creative element into your project submission? Did you deliver something of value to the end user (not just a login button and an index page)?
+
+Created a working Snakes and Ladders game. Though, I feel the UX would benefit from several more features. 
+
+###Code Quality: Did you follow code style guidance and best practices covered in class, such as spacing, modularity, and semantic naming? Did you comment your code as your instructors have in class?
+
+Tried to follow best preactices in the code 
+
+###Deployment: Did you deploy your application to a public url using GitHub Pages?
+
+Yes 
+
+Total: Your instructors will give you a total score on your project between:
--- js/script.js
+++ js/script.js
@@ -2,112 +2,145 @@
 // Set global variables
 var player = 0; 
 var currentPosition = 0;
-var rollResult;
+var rollResult = 0;
 
 // Assign IDs to cells, iterate, append with id
 function setCellIds() {
-  $.each($(".cell"), function(index, cellElement) {
-    var id = index + 1; 
-    $(cellElement).attr("id", id).append('<p>'+id+'</p>'); 
+  var rowNumber = 1;
+  var cellNumber = 100;
+  $(".board .row").each(function(index, el){
+    var $el = $(el);
+    $.each($el.find(".cell").get().reverse(), function(index, cellElement) {
+      $(cellElement).attr("id", cellNumber);//.append('<p>'+cellNumber+'</p>'); 
+      cellNumber = cellNumber - 1;
+    });
+    rowNumber = rowNumber + 1;    
   });
+
 }
 
-// Make snakesAndLadders Object with key : value
-var snakesAndLadders = {
-    12 : 16, 
-    19 : 22,
-
-    25 : 17,
-    28 : 20,
-
-    32 : 22, 
-    37 : 42,
-
-    47 : 27, 
-
-    55 : 76,
-
-    62 : 50,
-
-    78 : 97,
-
-    83 : 72, 
-    85 : 91,
-
-    99 : 80,
+var spot = {
+  // Snakes and Ladders
+  "3": {
+    "direction": "up",
+    "to": "15",
+    "type": "ladder"
+  },
+  "6": {
+    "direction": "up",
+    "to": "20",
+    "type": "ladder"
+  },
+  "20": {
+    "direction": "up",
+    "to": "30",
+    "type": "ladder"
+  },
+  "36": {
+    "direction": "up",
+    "to": "40",
+    "type": "ladder"
+  },
+  "63": {
+    "direction": "up",
+    "to": "75",
+    "type": "ladder"
+  },
+  "68": {
+    "direction": "up",
+    "to": "78",
+    "type": "ladder"
+  },
+  // snakes
+  "99": {
+    "direction": "down",
+    "to": "80",
+    "type": "snake"
+  },  
+  "91": {
+    "direction": "down",
+    "to": "77",
+    "type": "snake"
+  },
+  "87": {
+    "direction": "down",
+    "to": "69",
+    "type": "snake"
+  },
+  "65": {
+    "direction": "down",
+    "to": "58",
+    "type": "snake"
+  },
+  "47": {
+    "direction": "down",
+    "to": "37",
+    "type": "snake"
+  },
+  "34": {
+    "direction": "down",
+    "to": "22",
+    "type": "snake"
+  },
+  "25": {
+    "direction": "down",
+    "to": "20",
+    "type": "snake"
+  }
 };
 
 // Check current space for snake or ladder and move player
 function checkForSnakeOrLadder(nextPosition) {
-  if (nextPosition in snakesAndLadders) {
-    var newPosition = snakesAndLadders[nextPosition];
-    console.log("Transported to", newPosition);
-    nextPosition = newPosition; 
+  if (+nextPosition in spot) {
+    var newPosition = spot[nextPosition];
+    console.log("Transported to", newPosition.to);
+    $(".game-display").html("Transported to" + " ");
+    nextPosition = +newPosition.to; 
   }
   return nextPosition; 
 }
 
-// Place Snake and Ladder classes 
-function assignSnakesAndLadders() {
-  for(var key in snakesAndLadders) {
-    if (key < snakesAndLadders[key]) {
-      Object.keys(snakesAndLadders)
-      console.log('key: ' + key + '\n' + 'value: ' + snakesAndLadders[key]); 
-      $(key).addClass('ladder'); 
-
-      if (key === $('#')){
-        $(".cell").addClass('ladder'); 
-      }
-
-
-      
-      // ================= Help ================= //
-      // Use the value of key to to find matching .cell id number and add class .ladder
-      // Maybe something  with $('.cell').addClass('ladder'); ?
-
-    }
-    if (key < snakesAndLadders[key]) {
-      
-      // ================= Help ================= //
-      // Use the value of key to to find matching .cell id number and add class .snake
-
-    }
+function move(playr, move){
+  
+  // If player position is 0 (not on the board)
+  if ( playr === 0 ) {
+    // Add the ID to the rollREsult and add the player class
+    $('#'+move).addClass('player');
+    // var for current position to get the player class and assign the id 
+    currentPosition = 0;
+  }else{
+    // var for current position to get the player class and assign the id 
+    currentPosition = playr;
   }
+
+  // log the current position 
+  console.log(currentPosition);
+  $(".game-display").html("Current position" + " ");
+  // current position + new 
+  var nextPosition = +currentPosition + move;
+  // If your next position is more than 100, alert
+  if (nextPosition >= 100) {
+    $(".game-display").html("YOU WIN!");
+    return; 
+  }
+
+// Saving the next position in the console
+  console.log(nextPosition);
+  nextPosition = checkForSnakeOrLadder(nextPosition); 
+  // Clear player cell 
+  $('.player').removeClass('player'); 
+  // make new position .player 
+  // add new value to stored current position 
+  player = nextPosition;
+  $('#'+nextPosition).addClass('player');
 }
-
-assignSnakesAndLadders(); 
-
 
 // Function for roll 
 function roll() {
   rollResult = Math.floor((Math.random()*6)+1);
-    console.log("Roll was:", rollResult);
-  // If player position is 0 (not on the board)
-  if ( player === 0 ) {
-    // Add the ID to the rollREsult and add the player class
-    $('#'+rollResult).addClass('player');
-  }
-  player = rollResult;
-    
-    // var for current position to get the player class and assign the id 
-    currentPosition = $('.player').attr('id');
-    // log the current position 
-    console.log(currentPosition);
-    // current position + new 
-    var nextPosition = parseInt(currentPosition) + rollResult;
-    // If your next position is more than 100, alert
-    if (nextPosition >= 100) {
-      alert("YOU WIN"); 
-    }
-
-  // Saving the next position in the console
-    console.log(nextPosition);
-    nextPosition = checkForSnakeOrLadder(nextPosition); 
-    // Clear player cell 
-    $('.player').removeClass('player'); 
-    // make new position .player 
-    // add new value to stored current position 
-    $('#'+nextPosition).addClass('player');
+  console.log("Roll was:", rollResult);
+  $(".game-display").html("Roll was:" + rollResult);
+  move(player, rollResult);
 }
 
 // Function for calling roll() on click 
